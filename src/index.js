366ceeeccc35eeb97d0bc1be4f70b848d85b22c1
iamgaby <gaby.es.iaa@gmail.com>
2022-01-31 15:28:31 -0500

    Add scss files settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import './style.css';
+import './scss/style.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[sc]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
