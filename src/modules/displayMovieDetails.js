c4512e01e3aa02b89e53c012640197cd1b1b6f17
iamgaby <gaby.es.iaa@gmail.com>
2022-02-01 14:51:03 -0500

    Create functions to display comments and add new comments

--- src/modules/displayMovieDetails.js
+++ src/modules/displayMovieDetails.js
@@ -1,5 +1,10 @@
+import { getComments, addComment } from './consumeInvolvementAPI.js';
+
 const modalBox = document.querySelector('.modal__box');
 const modalCloseBtn = document.querySelector('#btn__close-modal');
+const commentForm = document.querySelector('#form__comment');
+const formMessage = document.querySelector('#form__message');
+let timerId = '';
 
 const arrIntoString = (arr) => {
   let str = '';
@@ -16,7 +21,7 @@ const buildMovieDescription = (data) => {
         <h2>${data.name}</h2>
         <ul class="movie__more">
           <li>
-            <span class="item-category">Gender:</span>
+            <span class="item-category">Genre:</span>
             <p>${arrIntoString(data.genres)}</p>
           </li>
           <li>
@@ -31,13 +36,99 @@ const buildMovieDescription = (data) => {
         ${data.summary}`;
 };
 
+const buildMovieComments = (arr) => {
+  const movieComments = document.querySelector('.movie__comments');
+  movieComments.innerHTML = '';
+  const title = document.createElement('h3');
+  title.textContent = `Comments (${arr.length})`;
+  movieComments.appendChild(title);
+  const commentList = document.createElement('ul');
+  commentList.classList.add('comments__list');
+  arr.forEach((comment) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = ` <div class="owner">
+              <span>${comment.creation_date}</span>
+              <span class="item-name">${comment.username}:</span>
+            </div>
+            <div class="comment">
+              <p class="item-comment">${comment.comment}</p>
+            </div>`;
+    commentList.appendChild(listItem);
+  });
+  movieComments.appendChild(commentList);
+};
+
 const displayMovieDetails = (movie) => {
   buildMovieDescription(movie);
+  getComments(movie.id).then((list) => {
+    buildMovieComments(list);
+  });
   modalBox.classList.add('modal__box-display');
 };
 
 modalCloseBtn.addEventListener('click', () => {
   modalBox.classList.remove('modal__box-display');
+});
+
+const displayMessage = (msg) => {
+  clearTimeout(timerId);
+  if (msg) {
+    formMessage.textContent = msg;
+    formMessage.classList.add('error-message');
+  }
+  formMessage.classList.add('visible');
+  timerId = setTimeout(() => {
+    formMessage.classList.remove('error-message', 'visible');
+  }, 5000);
+};
+
+const validString = (str) => {
+  if (str.match(/^[a-zA-Z0-9À-ÿ\u00f1\u00d1\u00E0\u00FC_\- ]{1,30}$/)) return true;
+  return false;
+};
+
+const validForm = (name, comment) => {
+  name.value = name.value.trim();
+  comment.value = comment.value.trim();
+  if (!validString(name.value)) {
+    name.focus();
+    displayMessage('Name field only allows alphanumeric, hyphens, underscores, and max 30 characters.');
+    return false;
+  }
+  if (comment.value === '' || comment.value.length > 250) {
+    comment.focus();
+    displayMessage('Insights field allows 1 to 250 characters.');
+    return false;
+  }
+  return true;
+};
+
+const sendComment = () => {
+  const name = document.querySelector('#inp__username');
+  const comment = document.querySelector('#inp__insights');
+  const movieId = document.querySelector('.movie__details').getAttribute('data-movieId');
+
+  if (validForm(name, comment)) {
+    addComment(movieId, name.value, comment.value).then((ans) => {
+      if (ans === 'Created') {
+        getComments(movieId).then((list) => {
+          buildMovieComments(list);
+        });
+        name.value = '';
+        name.focus();
+        comment.value = '';
+        formMessage.textContent = 'Comment sent successfully';
+        displayMessage();
+      } else {
+        displayMessage('Comments are not available for now. Try again later.');
+      }
+    });
+  }
+};
+
+commentForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendComment();
 });
 
 export { displayMovieDetails as default };
