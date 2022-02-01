88690b2493a2a14e120a3e94e225e5e6b65d22dc
iamgaby <gaby.es.iaa@gmail.com>
2022-02-01 08:53:33 -0500

    Add event to call display modal window in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './scss/style.scss';
+
+import { displayMovieDetails, createApp } from './modules/displayMovieDetails.js';
+
+window.addEventListener('load', displayMovieDetails(40995));
+// window.addEventListener('load', createApp('appTest'));
