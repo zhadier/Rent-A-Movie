7432f60941f9b3001b184ecd709705e465766ccb
iamgaby <gaby.es.iaa@gmail.com>
2022-02-01 17:50:07 -0500

    Fix conflicts and linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './scss/style.scss';
 import getMovieData from './modules/consumeTVMazeAPI.js';
 import displayMovies from './modules/displayMovies.js';
-import totalItems from './modules/allItemsCounter';
+import totalItems from './modules/allItemsCounter.js';
 
 const query = 'action';
 const homeItems = document.querySelector('.movie-section__items');
