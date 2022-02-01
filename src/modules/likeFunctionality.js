5e675ddbf05c5cff51281061cd9f159eda8e4d18
Zeeshan Haider <zhadier39@gmail.com>
2022-02-01 23:51:47 +0500

    add functionality to get and set npm start

--- src/modules/likeFunctionality.js
+++ src/modules/likeFunctionality.js
@@ -1,22 +1,19 @@
 const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 const appID = 'zggEBXzpFcQqjDxvMhMz';
 
-const setLikes = async (id) => {
+const setLike = async (id) => {
   const connect = await fetch(`${baseURL}${appID}/likes/`, {
     method: 'POST',
     body: JSON.stringify({ item_id: id }),
     headers: { 'Content-type': 'application/JSON' },
   });
-  const response = await connect.json();
+  const response = await connect.text();
   return response;
 };
 
-const getLikes = async () => {
-  const connect = await fetch(`${baseURL}${appID}/likes/`).then((r) => {
-    console.log(r);
-  });
+const getLike = async (id) => {
+  const connect = await fetch(`${baseURL}${appID}/likes?item_id=${id}`);
   const response = await connect.json();
-  console.log(response);
   return response;
 };
 // fetch(baseURL, {
@@ -24,4 +21,14 @@ const getLikes = async () => {
 //   headers: { 'Content-type': 'application/JSON' },
 // }).then((response) => console.log(response.text()));
 // // });
-export { getLikes, setLikes };
+
+const updateLikes = () => {
+  getLike().then((response) => {
+    response.forEach((element) => {
+      const container = document.querySelector(`#${element.item_id}`);
+      container.children[1].children[1].children[1].innerHTML = `${element.likes}`;
+    });
+  });
+};
+
+export { updateLikes, setLike };
