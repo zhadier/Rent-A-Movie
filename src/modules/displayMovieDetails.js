8f33e4f82e19ab185ad9c79b3481b7fd0cfdbf98
iamgaby <gaby.es.iaa@gmail.com>
2022-02-01 08:52:54 -0500

    Add function to display movie info in modal window

---
+++ src/modules/displayMovieDetails.js
@@ -0,0 +1,49 @@
+import getMovie from './consumeTVMazeAPI.js';
+import { newApp } from './consumeInvolvementAPI.js';
+
+const arrIntoString = (arr) => {
+  let str = '';
+  arr.forEach((el) => {
+    str += `${el} | `;
+  });
+  return str;
+};
+
+const buildMovieDescription = (data) => {
+  const movieDetails = document.querySelector('.movie__details');
+  movieDetails.setAttribute('data-movieid', data.id);
+  movieDetails.innerHTML = `<img src="${data.image.medium}" alt="${data.name} movie image">
+        <h2>${data.name}</h2>
+        <ul class="movie__more">
+          <li>
+            <span class="item-category">Gender:</span>
+            <p>${arrIntoString(data.genres)}</p>
+          </li>
+          <li>
+            <span class="item-category">Ended:</span>
+            <p>${data.ended}</p>
+          </li>
+          <li>
+            <span class="item-category">Raiting:</span>
+            <p>${data.rating.average}</p>
+          </li>
+        </ul>
+        ${data.summary}`;
+};
+
+const displayMovieDetails = (movieId) => {
+  getMovie(movieId).then((obj) => {
+    buildMovieDescription(obj, movieId);
+    // getComments(movieId).then((list) => {
+    //   buildMovieComments(list);
+    // });
+  });
+};
+
+const createApp = (app) => {
+  newApp(app).then((ans) => {
+    console.log(ans);
+  });
+};
+
+export { displayMovieDetails, createApp };
