510397df8625f7599f662fa474887059fa42386d
iamgaby <gaby.es.iaa@gmail.com>
2022-02-01 14:50:36 -0500

    Create functions to consume involvement API form comments

---
+++ src/modules/consumeInvolvementAPI.js
@@ -0,0 +1,26 @@
+const iBaseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/zggEBXzpFcQqjDxvMhMz/comments';
+
+const getComments = async (movieId) => {
+  const connect = await fetch(`${iBaseURL}?item_id=${movieId}`);
+  const commentsList = await connect.json();
+  if (commentsList.error) return [];
+  return commentsList;
+};
+
+const addComment = async (movieId, username, comment) => {
+  const connect = await fetch(`${iBaseURL}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: movieId,
+      username,
+      comment,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const response = await connect.text();
+  return response;
+};
+
+export { getComments, addComment };
