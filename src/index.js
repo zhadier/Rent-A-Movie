a51cf78fcc1c69ed7291f2c20d6f513236c8dd7b
Zeeshan Haider <zhadier39@gmail.com>
2022-02-02 01:05:11 +0500

    show items everytime page loads

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './scss/style.scss';
 import getMovieData from './modules/consumeTVMazeAPI.js';
 import displayMovies from './modules/displayMovies.js';
+import totalItems from './modules/allItemsCounter';
 
 const query = 'action';
-
+const homeItems = document.querySelector('.movie-section__items');
 getMovieData(0, query).then((movieList) => {
   displayMovies(movieList);
+  const all = document.querySelectorAll('.movie-wrapper');
+  totalItems(all, homeItems);
 });
